d23074bd050327c6e4e263b6041cf99538177a81
gem <gemalexandrag@gmail.com>
2021-02-19 18:32:38 -0700

    attempt to select square

--- Tetris.js
+++ Tetris.js
@@ -1,27 +1,31 @@
 //shapes
 let l = [
 [
-  [0,0,1],
-  [1,1,1],
-  [0,0,0]
+  [0,0,1,0],
+  [1,1,1,0],
+  [0,0,0,0],
+  [0,0,0,0]
 ],
 
 [
-  [0,1,0],
-  [0,1,0],
-  [0,1,1]
+  [0,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,0,0]
 ],
 
 [
-  [0,0,0],
-  [1,1,1],
-  [1,0,0]
+  [0,0,0,0],
+  [1,1,1,0],
+  [1,0,0,0],
+  [0,0,0,0]
 ],
 
 [
-  [1,1,0],
-  [0,1,0],
-  [0,1,0]
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,0,0],
+  [0,0,0,0]
 ]
 ];
 
@@ -57,32 +61,38 @@ let i = [
 
 let j = [
 [
-  [0,1,0],
-  [0,1,0],
-  [1,1,0]
+  [0,1,0,0],
+  [0,1,0,0],
+  [1,1,0,0],
+  [0,0,0,0]
 ],
 
 [
-  [1,0,0],
-  [1,1,1],
-  [0,0,0]
+  [1,0,0,0],
+  [1,1,1,0],
+  [0,0,0,0],
+  [0,0,0,0]
 ],
 
 [
-  [0,1,1],
-  [0,1,0],
-  [0,1,0]
+  [0,1,1,0],
+  [0,1,0,0],
+  [0,1,0,0],
+  [0,0,0,0]
 ],
 
 [
-  [0,0,0],
-  [1,1,1],
-  [0,0,1]
+
+  // select ex: j[3][0][1]
+  [0,0,0,0],
+  [1,1,1,0],
+  [0,0,1,0],
+  [0,0,0,0]
 ]
 ];
 
 
-
+//creates table
 $(document).ready(function(){
   console.log("in function");
   for(let i = 0; i < 20; i++){
@@ -91,10 +101,19 @@ $(document).ready(function(){
       row.insertCell();
     }
   }
+  let x = document.getElementById("table").childNodes[0];
+  let y = x.childNodes[1];
+  console.log(x)//.css("background-color:blue")
 });
-const getShape(a,b,c)=>{
-Math.floor(Math.random() * 3);
-
+//picks random shape
+const getShape = ()=>{
+let shapes = [l, i, j];
+return shapes[Math.floor(Math.random() * 3)];
 }
-    const show(getshape(l,i,j))=> show(id){
-       get(id).style.visibility = null;     }
+
+//  $( "table tr:first:nth-child(2)" ).css("background-color:blue");
+    const show = () =>{
+      for(i = 0; i < 7; i++){
+
+      }
+    }
